c98b2bbd422a11e34c6979747752f9b9677f8ae4
wojciech c <wciesielski@gmail.com>
2020-07-21 12:41:43 +0200

    Test names

--- src/test/java/com/excercise/game/pp1/Exercise1Test.java
+++ src/test/java/com/excercise/game/pp1/Exercise1Test.java
@@ -1,5 +1,6 @@
 package com.excercise.game.pp1;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,22 +9,31 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 class Exercise1Test {
 
-    Exercise1 sut = new Exercise1();
+  Exercise1 sut = new Exercise1();
 
-    @Test
-    void thirdIsFizz() {
-        String result = sut.generate(3);
+  @Test
+  @DisplayName("test 1 -> 1")
+  void firstIsOne() {
+    assertEquals("1", sut.generate(1));
+  }
 
-        assertEquals("Fizz", result);
-    }
+  @Test
+  @DisplayName("test 3 -> Fizz")
+  void thirdIsFizz() {
+    String result = sut.generate(3);
 
-    @Test
-    void fifthIsBuzz() {
-        fail("Not implemented");
-    }
+    assertEquals("Fizz", result);
+  }
 
-    @Test
-    void thirdAndFifthIsFizzBuzz() {
-        fail("Not implemented");
-    }
+  @Test
+  @DisplayName("test 5 -> Buzz")
+  void fifthIsBuzz() {
+    fail("Not implemented");
+  }
+
+  @Test
+  @DisplayName("test 15 -> Fizz\\nBuzz")
+  void thirdAndFifthIsFizzBuzz() {
+    fail("Not implemented");
+  }
 }
